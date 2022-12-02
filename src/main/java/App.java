3c5e10ff8b5e2c570685929f519cc0d231a4a3d6
Vladislav Shilov <Shilovvlad@mail.ru>
2022-12-02 17:54:08 +0300

    add Cat and bean Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorldBean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(helloWorldBean1.getMessage());
+
+        HelloWorld helloWorldBean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(helloWorldBean2.getMessage());
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1.getName());
+
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat2.getName());
+
+        System.out.println("HelloWorld is singleton? " + (helloWorldBean1 == helloWorldBean2));
+        System.out.println(helloWorldBean1 + " = " + helloWorldBean2);
+        System.out.println("Cat is singleton? " + (cat1 == cat2));
+        System.out.println(cat1 + " != " + cat2);
     }
 }
